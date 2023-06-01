f2c8cabe9d1073256bb01a897e21e76537d6a722
yenting <yenting@yentingdeMacBook-Pro.local>
2023-06-01 15:43:29 +0800

    feat: test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,16 +11,16 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://github.com",
+  url: "https://xxxxtim.github.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/Blog.github.io/",
+  baseUrl: "/xxxxtim.github.io/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "xxxxtim", // Usually your GitHub org/user name.
-  projectName: "Blog.github.io", // Usually your repo name.
-  deploymentBranch: "deploy",
+  projectName: "xxxxtim.github.io", // Usually your repo name.
+  deploymentBranch: "gh-pages",
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
