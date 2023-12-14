ba3ecd9ef43fb3edb6b1e43efb72bab67d34e3e8
yenting <yenting@yentingdeMacBook-Pro.local>
2023-12-15 01:34:37 +0800

    feat: 666

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Xtim's Blog",
+  title: "Xtim's blog",
   tagline: "Dinosaurs are cool",
   favicon: "img/ghost.png",
 
