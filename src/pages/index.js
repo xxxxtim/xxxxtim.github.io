dc4ae085f8bde3eb0a7569e9df89869ae3f31799
yenting <yenting@yentingdeMacBook-Pro.local>
2023-12-23 21:44:33 +0800

    feat: add goolge search console

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import Head from "@docusaurus/Head";
 import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
 // test
 export default function Home() {
   return (
     <Layout>
+      <meta
+        name="google-site-verification"
+        content="w5D743PiP-nV9ZnG0E5Toy5BvqICaFPVfmEpVf3gRDU"
+      />
       <main>
         <HomepageFeatures />
       </main>
