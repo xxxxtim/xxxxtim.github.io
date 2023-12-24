83d1c270b7f4313358642b46fd9351b3b9425e7c
yenting <yenting@yentingdeMacBook-Pro.local>
2023-12-25 00:09:19 +0800

    fix: change the production url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: "img/ghost.png",
 
   // Set the production url of your site here
-  url: "https://xxxxtim.github.com",
+  url: "https://xxxxtim-github-io.vercel.app/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
